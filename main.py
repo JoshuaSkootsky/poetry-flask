e0010a10341cb840036392f437daedea7062fc82
Joshua Skootsky <joshua.skootsky@gmail.com>
2021-01-25 09:42:42 -0500

    okay, added a route for my name

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "Hello, World!"
+
+@app.route("/joshua")
+def joshua():
+    return "Hello there, Joshua..."
     
 if __name__ == "__main__":
     app.run(debug=True)
